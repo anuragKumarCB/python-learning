0fe2a2eeab17d9897f5342848b42e36fa770c68d
anuragKumarCB <anuragkumar.cb01@gmail.com>
2024-02-15 19:24:55 +0530

    changed the file anme and added type-method in hello_python2 file

--- 01_basic/02_hello_python2.py
+++
@@ -1,3 +0,0 @@
-from hello_python import greet
-
-greet("hello", "OddOne")
---
+++ 01_basic/hello_python2.py
@@ -0,0 +1,14 @@
+from hello_python import greet
+
+greet("hello", "OddOne")
+
+# By using type() method we can check for data-types.
+
+str1 = 'tea'
+num1 = 456
+type(str1)
+# => <class 'str'>
+type(num1)
+# => <class 'int'>
+type({})
+# => <class 'dict'>     empty { curly-braces} is treated as Dictionary
