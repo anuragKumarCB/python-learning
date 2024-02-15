390729f2850e8f10f910188a40131b46a3071140
anuragKumarCB <anuragkumar.cb01@gmail.com>
2024-02-15 19:27:02 +0530

    added comparision file

---
+++ 02_data-type/06_comparision.py
@@ -0,0 +1,30 @@
+x = 2
+y = 3
+z = 3
+
+# comparison operators are binary operators that need two
+# operands to do the operation and give back the result a Boolean.
+
+# greaterThan -
+y > x       # (3 is greaterThan 2)
+# => true
+
+# greaterThan-equalTo -
+y >= x       # (3 is greater than equal to 2)
+# => true
+
+# lessThan -
+y < x       # (3 is lessThan 2)
+# => False
+
+# lessThan-equalTo -
+y <= x       # (3 is less than equal to 2)
+# => False
+
+# equalTo -
+y == z      # (3 is equal to 3)
+# => True
+
+# not(!)
+y != z      # (3 is not equal to 3)
+# => False
